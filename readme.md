ae0e704ebf012d2287b2d613ea98eaaa4f044721
devjanaprime <dev@primeacademy.io>
2016-09-20 11:28:30 -0500

    basicSetup

---
+++ readme.md
@@ -0,0 +1,4 @@
+Pi In Class Node Express Angular Intro
+======================================
+
+9-20-2016
---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require( 'express' );
+var app = express();
+var path = require( 'path' );
+var bodyParser = require ( 'body-parser' );
+var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var port = process.env.PORT || 3208;
+
+//spin up server
+app.listen( port, function(){
+  console.log( 'server up on', port );
+}); // end server up
+
+// base url
+app.get( '/', function( req, res ){
+  console.log( 'base url hit' );
+  res.sendFile( path.resolve( 'public/index.html' ) );
+}); // end base url
+
+// static folder
+app.use( express.static( 'public' ) );
