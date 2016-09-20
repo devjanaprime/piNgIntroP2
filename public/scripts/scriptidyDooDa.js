e85d19d9886598b6926c10981c721bf08d61cee6
devjanaprime <dev@primeacademy.io>
2016-09-20 11:42:03 -0500

    ngClickModelExpression

--- public/scripts/scriptidyDooDa.js
+++ public/scripts/scriptidyDooDa.js
@@ -4,4 +4,8 @@ var myApp = angular.module( 'myApp', [] );
 
 myApp.controller( 'poeDameron', [ '$scope', function( $scope ){
   console.log( 'NG' );
+
+  $scope.addSong = function(){
+    console.log( 'in addSong:', $scope.badSong );
+  }; // end add song
 }]); // end controller
