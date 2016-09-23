222d146b21778f8dea619af14449ef1992fdbe71
devjanaprime <dev@primeacademy.io>
2016-09-23 16:11:38 -0500

    added ng-repeat

--- public/scripts/scriptidyDooDa.js
+++ public/scripts/scriptidyDooDa.js
@@ -2,10 +2,8 @@ console.log( 'ls' );
 
 var myApp = angular.module( 'myApp', [] );
 
-var allSongs=[];
 myApp.controller( 'poeDameron', [ '$scope', '$http', function( $scope, $http ){
-  console.log( 'NG' );
-
+  $scope.allSongs=[];
   $scope.addSong = function(){
     console.log( 'in addSong:', $scope.badSong );
     // can we put the bad song in an object?
@@ -13,18 +11,15 @@ myApp.controller( 'poeDameron', [ '$scope', '$http', function( $scope, $http ){
       title: $scope.badSong,
       artist: $scope.badArtist
     }; // end new song
-    console.log( 'sending:', newSong );
     // test send via http to post route
     $http({
       method: 'POST',
       url: '/testPost',
       data: newSong
     }).then(function( response ){
-      console.log( 'back from server with:', response );
     })
-
     // can we push the bad song object into our array?
-    allSongs.push( newSong );
-    console.log( 'allSongs:', allSongs );
+    $scope.allSongs.push( newSong );
+    console.log( 'allSongs:', $scope.allSongs );
   }; // end add song
 }]); // end controller
